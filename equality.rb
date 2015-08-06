e83f22b8f2b740104619981361db9281b450cea5
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-06 14:01:01 +0100

    Refactored

--- equality.rb
+++ equality.rb
@@ -10,13 +10,17 @@ class Ship
    end
 
   def eql?(other)
-    self.size == other.size && other.class == self.class
+    self.size == other.size
   end
 
   def hash
-    (self.size.to_s + self.class.object_id.to_s).hex
+    self.class.object_id
   end
 end
+
+p Ship.new(2).hash
+p Ship.new(2).hash
+p Ship.new(1).hash
 
 p [Ship.new(1), Ship.new(2)] & [Ship.new(2), Ship.new(3)]
 
